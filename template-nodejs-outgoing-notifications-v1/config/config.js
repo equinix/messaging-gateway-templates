4f3c07d500df7c1787f457aeb9d8942e16f95bef
ravikumar-equinix <rchandran@ap.equinix.com>
2021-05-20 16:57:17 +0800

    Updated Config file with appropriate placeholder

---
+++ template-nodejs-outgoing-notifications-v1/config/config.js
@@ -0,0 +1,7 @@
+'use strict';
+//Below configurations will be issued to customer offline as part of on-boarding process.
+module.exports = {
+    EQUINIX_OUTGOING_QUEUE: '<EQUINIX_OUTGOING_QUEUE>',
+    EQUINIX_OUTGOING_QUEUE_CONNECTION_STRING: '<EQUINIX_OUTGOING_QUEUE_CONNECTION_STRING>',
+    FILE_STORAGE_KEY : '<FILE_STORAGE_KEY>'
+}
