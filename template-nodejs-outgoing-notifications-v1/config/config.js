76badfeae56fbeb1dbfb23773b207540d8e7005c
ravikumar-equinix <rchandran@ap.equinix.com>
2021-05-20 16:55:44 +0800

    Deleting Config

--- template-nodejs-outgoing-notifications-v1/config/config.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-//Below configurations will be issued to customer offline as part of on-boarding process.
-module.exports = {
-    EQUINIX_OUTGOING_QUEUE: 'uat_e_queue',
-    EQUINIX_OUTGOING_QUEUE_CONNECTION_STRING: 'Endpoint=sb://eqixsvcbus.servicebus.windows.net/;SharedAccessKeyName=uat_listenonly;SharedAccessKey=NJQ3PB9h9H9ArZTenp3JeXvqZ9jnDiZWVOLKBSUG07o=;EntityPath=uat_e_queue',
-    FILE_STORAGE_KEY : '<FILE_STORAGE_KEY>'
-}
