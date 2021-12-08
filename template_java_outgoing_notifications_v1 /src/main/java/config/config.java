4d734f40a868297c5efefb6f0aec79261e094a3f
ravikumar-equinix <rchandran@ap.equinix.com>
2021-12-08 22:29:44 +0800

    Delete template_java_outgoing_notifications_v1  directory

--- template_java_outgoing_notifications_v1 /src/main/java/config/config.java
+++
@@ -1,9 +0,0 @@
-package config;
-
-public class config {
-    //Below configurations will be issued to customer offline as part of on-boarding process.
-    public static final String EQUINIX_OUTGOING_QUEUE_CONNECTION_STRING = "<EQUINIX_OUTGOING_QUEUE_CONNECTION_STRING>";
-    public static final String EQUINIX_OUTGOING_QUEUE = "<EQUINIX_OUTGOING_QUEUE>";
-    public static final String FILE_STORAGE_DOWNLOAD_KEY = "<FILE_STORAGE_DOWNLOAD_KEY>";
-
-}
